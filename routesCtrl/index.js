13d749c1a2039d444471593c96658b1e6cd6be02
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-01 20:16:08 -0400

    swaggerupdate

--- routesCtrl/index.js
+++ routesCtrl/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const usersRoute = require('./users');
+const swaggerRoute = require('./swagger');
+
 router.get('/', (req,res) => {
     res.send('HOMEPAGE')
 });
+
+router.use('/users', usersRoute);
+
+router.use('/apiDocs', swaggerRoute);
+
 
 module.exports = router;
