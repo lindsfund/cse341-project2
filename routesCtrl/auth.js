827ec3a1e92cc042cdacf79d8e3eb4d8f785b876
lindsfund <71404773+lindsfund@users.noreply.github.com>
2023-07-03 17:54:54 -0400

    got token from auth

--- routesCtrl/auth.js
+++ routesCtrl/auth.js
@@ -4,23 +4,34 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('in the authRoute');
+});
+
 router.post('/', async (req, res) => {
     //get login data from body
     const userLogin = [{userName: "Testing", password: "Testing", roles:["admin", "editor", "viewer"]}]
 
     //check user data against database
-    let user = userLogin.find(u => u.userName === req.body.userName);
-    if(!user) throw new Error('invalid UserName');
+    try {
+        userLogin.find(u => u.userName === req.body.userName);
+    } catch (error) {
+        //invalid userName
+        res.status(400).json({message: error.message});
+    }
 
     //check password against database
-    let pswd = userLogin.find(u => u.password === req.body.password);
-    if(!password) throw new Error('invalid password');
-
+    try {
+        userLogin.find(u => u.password === req.body.password);
+    } catch (error) {
+        //invalid password
+        res.status(400).json({message: error.message});
+    }
 
     //assign token
     const token = jwt.sign({
-        id: user.userName,
-        roles: user.roles,
+        id: userLogin.userName,
+        roles: userLogin.roles,
     }, "jwtPrivateKey", {expiresIn: '15m'});
 
     //send out token
